81968d1f054270b75bb610971ca778a24535e2c3
Karthik <karthik.nishanth06@gmail.com>
2023-03-08 20:50:36 +0000

    initial project

---
+++ src/pages/sign.js
@@ -0,0 +1,303 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+function sign() {
+  const [signin, setSignin] = useState(true);
+  const buttvariant = {
+    signin: { opacity: 1, x: -10 },
+    signup: { opacity: 0.5, x: 10 },
+  };
+  return (
+    <>
+      <title>Sign In | Sign Up</title>
+      <section className="bg-white ">
+        <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
+          <form className="w-full max-w-md">
+            <div className="flex justify-center mx-auto">
+              {/* <img
+                className="w-auto h-7 sm:h-8"
+                src="https://merakiui.com/images/logo.svg"
+                alt=""
+              /> */}
+            </div>
+            {console.log(signin)}
+            <div className="flex items-center justify-center mt-6">
+              <motion.button
+                animate={signin ? "signin" : "signup"}
+                variants={buttvariant}
+                type="button"
+                onClick={() => setSignin(true)}
+                className={`w-1/3 pb-4 font-medium text-center  ${
+                  signin
+                    ? "text-gray-800 border-b-2 border-blue-500"
+                    : "text-gray-500"
+                } capitalize border-b `}
+              >
+                sign in
+              </motion.button>
+
+              <motion.button
+                animate={signin ? "signup" : "signin"}
+                variants={buttvariant}
+                type="button"
+                onClick={() => setSignin(false)}
+                className={`w-1/3 pb-4 font-medium text-center ${
+                  signin
+                    ? "text-gray-500"
+                    : "text-gray-800 border-b-2 border-blue-500"
+                } capitalize border-b `}
+              >
+                sign up
+              </motion.button>
+            </div>
+
+            {signin == true ? (
+              <>
+                <div className="relative flex items-center mt-8">
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-3 text-gray-300 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                  </span>
+
+                  <input
+                    type="text"
+                    className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11  focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Username/Email"
+                  />
+                </div>
+
+                <div className="relative flex items-center mt-4">
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-3 text-gray-300 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                      />
+                    </svg>
+                  </span>
+
+                  <input
+                    type="password"
+                    className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg   focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Password"
+                  />
+                </div>
+
+                <div className="mt-6">
+                  <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                    Sign In
+                  </button>
+
+                  <div className="mt-6 text-center ">
+                    <button
+                      type="button"
+                      onClick={() => setSignin(false)}
+                      className="text-sm text-blue-500 hover:underline "
+                    >
+                      Don't have an account?
+                    </button>
+                  </div>
+                  <div className="flex items-center justify-between mt-4">
+                    <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/5"></span>
+
+                    <a
+                      href="#"
+                      className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
+                    >
+                      or login with Social Media
+                    </a>
+
+                    <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/5"></span>
+                  </div>
+                  <div className="flex items-center mt-6 -mx-2">
+                    <button
+                      type="button"
+                      className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none"
+                    >
+                      <svg
+                        className="w-4 h-4 mx-2 fill-current"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
+                      </svg>
+
+                      <span className="hidden mx-2 sm:inline">
+                        Sign in with Google
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="relative flex items-center mt-8">
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-3 text-gray-300 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                  </span>
+
+                  <input
+                    type="text"
+                    className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11  focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Username"
+                  />
+                </div>
+
+                <div className="relative flex items-center mt-6">
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-3 text-gray-300 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                      />
+                    </svg>
+                  </span>
+
+                  <input
+                    type="email"
+                    className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11  focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Email address"
+                  />
+                </div>
+
+                <div className="relative flex items-center mt-4">
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-3 text-gray-300 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                      />
+                    </svg>
+                  </span>
+
+                  <input
+                    type="password"
+                    className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg   focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Password"
+                  />
+                </div>
+
+                <div className="relative flex items-center mt-4">
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-3 text-gray-300 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                      />
+                    </svg>
+                  </span>
+
+                  <input
+                    type="password"
+                    className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg   focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Confirm Password"
+                  />
+                </div>
+
+                <div className="mt-6">
+                  <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                    Sign Up
+                  </button>
+                  <div className="mt-6 text-center ">
+                    <button
+                      onClick={() => setSignin(true)}
+                      className="text-sm text-blue-500 hover:underline "
+                    >
+                      Already have an account?
+                    </button>
+                  </div>{" "}
+                  <div className="flex items-center justify-between mt-4">
+                    <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/5"></span>
+
+                    <a
+                      href="#"
+                      className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
+                    >
+                      or login with Social Media
+                    </a>
+
+                    <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/5"></span>
+                  </div>
+                  <div className="flex items-center mt-6 -mx-2">
+                    <button
+                      type="button"
+                      className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none"
+                    >
+                      <svg
+                        className="w-4 h-4 mx-2 fill-current"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
+                      </svg>
+
+                      <span className="hidden mx-2 sm:inline">
+                        Sign up with Google
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </>
+            )}
+          </form>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default sign;
