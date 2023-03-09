94e344b919c2863b4deef86a162a08b1240933c4
Karthik <karthik.nishanth06@gmail.com>
2023-03-09 16:13:08 +0000

    logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,30 @@
 import Link from "next/link";
 import logo from "../assets/logo.png";
+import Router from "next/router";
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 const Navbar = () => {
   useEffect(() => {
     const w = window.innerWidth;
+    let token = localStorage.getItem("token");
+    console.log(token);
     if (w >= 1024) {
       setIsOpen(true);
     }
+    if (token !== null) {
+      console.log("hi");
+      setSigned(true);
+    }
     return () => {};
   }, []);
-
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    Router.push("/");
+    Router.reload();
+  };
   const [isOpen, setIsOpen] = useState(false);
+  const [signed, setSigned] = useState(false);
   return (
     <nav className="relative bg-white shadow">
       <div className="container px-6 py-4 mx-auto">
@@ -94,51 +107,62 @@ const Navbar = () => {
                 >
                   Contact
                 </Link>
-                <Link
-                  href="/sign"
-                  className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0  hover:bg-gray-100 "
-                >
-                  Signin/Register
-                </Link>
-              </div>
-
-              <div className="flex items-center mt-4 lg:mt-0">
-                <button
-                  className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block  hover:text-gray-700  focus:text-gray-700 focus:outline-none"
-                  aria-label="show notifications"
-                >
-                  <svg
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                {!signed ? (
+                  <Link
+                    href="/sign"
+                    className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0  hover:bg-gray-100 "
                   >
-                    <path
-                      d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </button>
-
-                <button
-                  type="button"
-                  className="flex items-center focus:outline-none"
-                  aria-label="toggle profile dropdown"
-                >
-                  <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
-                    <img
-                      src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-                      className="object-cover w-full h-full"
-                      alt="avatar"
-                    />
-                  </div>
-
-                  <h3 className="mx-2 text-gray-700 lg:hidden">User</h3>
-                </button>
+                    Signin/Register
+                  </Link>
+                ) : (
+                  <button
+                    onClick={logout}
+                    className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0  hover:bg-gray-100 "
+                  >
+                    Signout
+                  </button>
+                )}
               </div>
+
+              {signed && (
+                <div className="flex items-center mt-4 lg:mt-0">
+                  <button
+                    className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block  hover:text-gray-700  focus:text-gray-700 focus:outline-none"
+                    aria-label="show notifications"
+                  >
+                    <svg
+                      className="w-6 h-6"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+
+                  <button
+                    type="button"
+                    className="flex items-center focus:outline-none"
+                    aria-label="toggle profile dropdown"
+                  >
+                    <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
+                      <img
+                        src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                        className="object-cover w-full h-full"
+                        alt="avatar"
+                      />
+                    </div>
+
+                    <h3 className="mx-2 text-gray-700 lg:hidden">User</h3>
+                  </button>
+                </div>
+              )}
             </motion.div>
           )}
         </div>
