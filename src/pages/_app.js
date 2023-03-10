a2497f025d0eed8f8d54a9236f5e2a6ed6320005
Karthik <karthik.nishanth06@gmail.com>
2023-03-10 11:27:37 +0000

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import { initGA, logPageView } from "../lib/ga";
 import Footer from "@/components/Footer";
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
   return (
     <>
       <Navbar />
