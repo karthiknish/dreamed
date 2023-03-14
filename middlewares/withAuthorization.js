83de427b4307dbefaf9cbb17f33ba891bc61807d
Karthik <karthik.nishanth06@gmail.com>
2023-03-14 13:25:54 +0000

    dwd

--- middlewares/withAuthorization.js
+++ middlewares/withAuthorization.js
@@ -6,7 +6,7 @@ export default function withAuthorization(middleware, requireAuth) {
     const pathname = request.nextUrl.pathname;
     console.log(requireAuth);
    console.log(requireAuth.some((path) => pathname.startsWith(path)));
-    if (requireAuth.some((path) => pathname.startsWith(path))) {
+    if (requireAuth?.some((path) => pathname.startsWith(path))) {
       const token = await getToken({
         req: request,
         secret: process.env.NEXTAUTH_SECRET,
@@ -17,7 +17,7 @@ export default function withAuthorization(middleware, requireAuth) {
         url.searchParams.set("callbackUrl ", encodeURI(request.url));
         return NextResponse.redirect(url);
       }
-   
+
       if (token.name !== "admin" && pathname === "/admin") {
         const url = new URL(`/403`, request.url);
         return NextResponse.rewrite(url);
