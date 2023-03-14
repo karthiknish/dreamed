19a84ddda578c9f444972938842edc344880043b
Karthik <karthik.nishanth06@gmail.com>
2023-03-14 13:41:23 +0000

    dwedf

--- middlewares/withAuthorization.js
+++ middlewares/withAuthorization.js
@@ -4,22 +4,20 @@ import { NextResponse } from "next/server";
 export default function withAuthorization(middleware, requireAuth) {
   return async (request, next) => {
     const pathname = request.nextUrl.pathname;
-   console.log(requireAuth?.some((path) => pathname.startsWith(path)));
+
     if (requireAuth?.some((path) => pathname.startsWith(path))) {
       const token = await getToken({
         req: request,
         secret: process.env.NEXTAUTH_SECRET,
       });
       console.log(token);
-      // if (!token) {
-      //   const url = new URL(`/api/auth/signin`, request.url);
-      //   url.searchParams.set("callbackUrl ", encodeURI(request.url));
-      //   return NextResponse.redirect(url);
-      // }
-
-      if (token.name !== "admin" && pathname === "/admin") {
+      if (token?.name !== "admin" && pathname === "/admin") {
         const url = new URL(`/403`, request.url);
         return NextResponse.rewrite(url);
+      } else if (token === null) {
+        const url = new URL(`/api/auth/signin`, request.url);
+        url.searchParams.set("callbackUrl ", encodeURI(request.url));
+        return NextResponse.redirect(url);
       }
     }
     return middleware(request, next);
--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,6 @@ import withAuthorization from "middlewares/withAuthorization";
 import { NextResponse } from "next/server";
 const mainMiddleware = (request) => {
   const res = NextResponse.next();
-  console.log(res);
   return res;
 };
 export default withAuthorization(mainMiddleware, ["/admin", "/dashboard"]);
