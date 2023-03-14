4e0f9d75fa20bf1c7a8ecc07270d22b82e239101
Karthik <karthik.nishanth06@gmail.com>
2023-03-14 13:04:01 +0000

    dwqf

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,7 @@ import withAuthorization from "middlewares/withAuthorization";
 import { NextResponse } from "next/server";
 const mainMiddleware = (request) => {
   const res = NextResponse.next();
+  console.log(res);
   return res;
 };
 export default withAuthorization(mainMiddleware, ["/admin", "/dashboard"]);
