fc6fe54532ba54075510ba650878c99a7d6c01a1
Karthik <karthik.nishanth06@gmail.com>
2023-03-15 21:15:29 +0000

    blog

--- src/pages/admin/blog/create.js
+++ src/pages/admin/blog/create.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import Markdown from "../../../components/Markdown";
 function Create() {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
@@ -39,13 +39,8 @@ return (
       className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg  focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
       placeholder="Image URL"
     />
-    <textarea
-      value={content}
-      type="text"
-      onChange={(e) => setContent(e.target.value)}
-      className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg  focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-      placeholder="Content"
-    />
+    <Markdown value={content} onChange={setContent} />
+
     <button onClick={handleSubmit}>Submit</button>
   </div>
 );
--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -1,6 +1,17 @@
 import dbConnect from "../../lib/dbConnect";
 import User from "../../models/User";
 import nodemailer from "nodemailer";
+const path = require("path");
+const hbs = require("nodemailer-express-handlebars");
+const handlebarOptions = {
+  viewEngine: {
+    extName: ".handlebars",
+    partialsDir: path.resolve("./src/templates/"),
+    defaultLayout: false,
+  },
+  viewPath: path.resolve("./src/templates/"),
+  extName: ".handlebars",
+};
 export default async function handler(req, res) {
   const { method } = req;
   await dbConnect();
@@ -30,38 +41,8 @@ export default async function handler(req, res) {
             from: process.env.NOREPLY_EMAIL,
             to: email,
             subject: "Welcome to Dream Education Consultancy",
-            html: `
-            <html><head> <script src="https://cdn.tailwindcss.com"></script></head>
-            <body><section class="max-w-2xl px-6 py-8 mx-auto bg-white dark:bg-gray-900">
-            <header>
-                <a href="#">
-                    <img class="w-auto h-7 sm:h-8" src="https://merakiui.com/images/full-logo.svg" alt="">
-                </a>
-            </header>
-        
-            <main class="mt-8">
-                <h2 class="text-gray-700 dark:text-gray-200">Hi ${name},</h2>
-        
-                <p class="mt-2 leading-loose text-gray-600 dark:text-gray-300">
-                    Dream education consultancy has invited you to Signup on <span class="font-semibold ">dreamedconsultancy.com</span>.
-                </p>
-                
-                <button class="px-6 py-2 mt-4 text-sm font-medium tracking-wider text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-                    Accept the invite
-                </button>
-                
-                <p class="mt-8 text-gray-600 dark:text-gray-300">
-                    Thanks, <br>
-                    Dream Ed Team
-                </p>
-            </main>
-            
-        
-            <footer class="mt-8">
-        
-                <p class="mt-3 text-gray-500 dark:text-gray-400">© ${new Date().getFullYear()} Dream Ed. All Rights Reserved.</p>
-            </footer>
-        </section></body></html>`,
+            template: "invitation",
+            context: { name },
           };
           await transporter.sendMail(mailOptions, function (err, info) {
             console.log(info);
