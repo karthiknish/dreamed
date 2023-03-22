64a53886db980e1e1fe46451b8b4f6cdb6bc0e9a
Karthik <karthik.nishanth06@gmail.com>
2023-03-22 00:22:33 +0000

    analytics done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,22 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import "../components/markdown.css";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { initGA, logPageView } from "../lib/ga";
 import Footer from "@/components/Footer";
 import { SessionProvider } from "next-auth/react";
-import Chat from "@/components/Chat";
 import Assistant from "@/components/assistant/Assistant";
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   useEffect(() => {
-    if (!window.GA_INITIALIZED) {
-      initGA();
-      window.GA_INITIALIZED = true;
-    }
+    initGA();
     logPageView();
-  }, []);
+    router.events.on("routeChangeComplete", logPageView);
+    return () => {
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, [router]);
   return (
     <SessionProvider>
       <Navbar />
